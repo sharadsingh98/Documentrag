de541d594553ef8f687ec8527a3fc200e94b7344
Sharad Singh <sharad_singh98.com>
2026-01-18 12:41:51 +1100

    Update Streamlit app with fixed imports

--- st_integration.py
+++ st_integration.py
@@ -11,9 +11,9 @@ from sqlalchemy import create_engine, inspect, text
 from typing import Dict, List, Optional, Tuple
 import json
 from datetime import datetime
-import plotly.express as px
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
+#import plotly.express as px
+#import plotly.graph_objects as go
+#from plotly.subplots import make_subplots
 import re
 
 from src.config.config import Config
